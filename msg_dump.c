d5e7763e73e6f06b6b3f9809d6d15762e6e1441d
Champ Yen <champ.yen@gmail.com>
2024-06-03 00:39:43 +0800

    Add files via upload
    * clean code
    * make section message dump a routine function "dump_msg"

--- msg_dump.c
+++ msg_dump.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <string.h>
 #include <sys/stat.h>
 #include <unistd.h>
@@ -12,10 +13,28 @@ long get_file_size(char *filename) {
     return file_status.st_size;
 }
 
+void dump_msg(char *msg_fn, uint8_t *rom_data, uint16_t *char_table, int start_ofs, int end_ofs)
+{
+    FILE *msg_fp = fopen(msg_fn, "w");
+    for(int i = start_ofs; i < end_ofs; i++)
+    {
+        uint8_t msg = rom_data[i];
+        if(msg < 0xE0) {
+            fwrite(&(char_table[msg]), 1, sizeof(short), msg_fp);
+        } else if(msg < 0xF0){
+            int tab_ofs = ((msg - 0xDF) << 8) | rom_data[++i];
+            fwrite(&(char_table[tab_ofs]), 1, sizeof(short), msg_fp);
+        } else {
+            // CMD
+        }
+    }
+    fclose(msg_fp);
+}
+
 #define MAX_LEN 1024
 int main(int argc, char ** argv)
 {
-    unsigned short char_table[0xC00] = {0};
+    uint16_t char_table[0xC00] = {0};
 
     char *charlist_fn = argv[1];
     FILE *charlist_fp = fopen(charlist_fn, "r");
@@ -34,7 +53,7 @@ int main(int argc, char ** argv)
         long num = strtol(strchr(strbuf, ',')+1, NULL, 16)+1;
         //printf("offset=%lX, num=%ld\n", offset, num);
         for(int i = 0; i < num; i++, offset+=0x10){
-            unsigned short charenc = 0;
+            uint16_t charenc = 0;
             fread(&charenc, 1, 2, charlist_fp);
             char_table[(offset - 0x90010) >> 4] = charenc;
             //printf("TAB[%lX] %X\n", (offset - 0x90010) >> 4, charenc);
@@ -46,7 +65,7 @@ int main(int argc, char ** argv)
 
     char *srom_fn = argv[2];
     long rom_size = get_file_size(srom_fn);
-    unsigned char *rom_data = malloc(rom_size);
+    uint8_t *rom_data = malloc(rom_size);
     FILE *romfp = fopen(srom_fn, "rb");
     if(romfp == NULL){
         printf("open %s failed\n", srom_fn);
@@ -55,72 +74,10 @@ int main(int argc, char ** argv)
     fread(rom_data, 1, rom_size, romfp);
     fclose(romfp);
 
-    FILE *msg_fp = fopen("msg1.txt", "wb");
-    int msg_offset = 0x54410;
-    for(int i = msg_offset; i < 0x56C10; i++)
-    {
-        unsigned char msg = rom_data[i];
-        if(msg < 0xE0) {
-            fwrite(&(char_table[msg]), 1, sizeof(short), msg_fp);
-        } else if(msg < 0xF0){
-            int tab_ofs = ((msg - 0xDF) << 8) | rom_data[++i];
-            fwrite(&(char_table[tab_ofs]), 1, sizeof(short), msg_fp);
-        } else {
-            // CMD
-        }
-    }
-    fclose(msg_fp);
-
-    msg_fp = fopen("msg2.txt", "wb");
-    msg_offset = 0x58410;
-    for(int i = msg_offset; i < 0x592D8; i++)
-    {
-        unsigned char msg = rom_data[i];
-
-        if(msg < 0xE0) {
-            fwrite(&(char_table[msg]), 1, sizeof(short), msg_fp);
-        } else if(msg < 0xF0){
-            int tab_ofs = ((msg - 0xDF) << 8) | rom_data[++i];
-            fwrite(&(char_table[tab_ofs]), 1, sizeof(short), msg_fp);
-        } else {
-            // CMD
-        }
-    }
-    fclose(msg_fp);
-
-    msg_fp = fopen("msg3.txt", "wb");
-    msg_offset = 0x5C410;
-    for(int i = msg_offset; i < 0x5D51B; i++)
-    {
-        unsigned char msg = rom_data[i];
-
-        if(msg < 0xE0) {
-            fwrite(&(char_table[msg]), 1, sizeof(short), msg_fp);
-        } else if(msg < 0xF0){
-            int tab_ofs = ((msg - 0xDF) << 8) | rom_data[++i];
-            fwrite(&(char_table[tab_ofs]), 1, sizeof(short), msg_fp);
-        } else {
-            // CMD
-        }
-    }
-    fclose(msg_fp);
-
-    msg_fp = fopen("msg4.txt", "wb");
-    msg_offset = 0x60410;
-    for(int i = msg_offset; i < 0x61BF6; i++)
-    {
-        unsigned char msg = rom_data[i];
-
-        if(msg < 0xE0) {
-            fwrite(&(char_table[msg]), 1, sizeof(short), msg_fp);
-        } else if(msg < 0xF0){
-            int tab_ofs = ((msg - 0xDF) << 8) | rom_data[++i];
-            fwrite(&(char_table[tab_ofs]), 1, sizeof(short), msg_fp);
-        } else {
-            // CMD
-        }
-    }
-    fclose(msg_fp);
+    dump_msg("msg1.txt", rom_data, char_table, 0x54410, 0x56C10);
+    dump_msg("msg2.txt", rom_data, char_table, 0x58410, 0x592D8);
+    dump_msg("msg3.txt", rom_data, char_table, 0x5C410, 0x5D51B);
+    dump_msg("msg4.txt", rom_data, char_table, 0x60410, 0x61BF6);
 
     free(rom_data);
     return 0;
